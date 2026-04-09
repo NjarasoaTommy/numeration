a2016d21469b687ef438dc3d70272061f448e983
Njarasoa Tommy <njarasoatommy@gmail.com>
2026-04-09 21:25:30 +0300

    Now, add a function which transform decimal number into a binary number.

--- main.py
+++ main.py
@@ -1,3 +1,8 @@
 from outils.binaire import *
-print("10 en binaire vaux", binary_to_decimal(10), "en décimal.")
-print("1110 en binaire vaux", binary_to_decimal(1110), "en décimal.")
+# print("10 en binaire vaut", binary_to_decimal(10), "en décimal.")
+# print("1110 en binaire vaut", binary_to_decimal(1110), "en décimal.")
+
+print("3 en décimale vaut", decimal_to_binary(3), "en binaire.")
+print("15 en décimale vaut", decimal_to_binary(15), "en binaire.")
+print("75 en décimale vaut", decimal_to_binary(75), "en binaire.")
+print("16 en décimale vaut", decimal_to_binary(16), "en binaire.")
--- outils/binaire.py
+++ outils/binaire.py
@@ -7,3 +7,24 @@ def binary_to_decimal(binary_number):
         decimal_number += unity * (2 ** power)
         power += 1
     return decimal_number
+
+def decimal_to_binary(decimal_number):
+    if decimal_number == 2:
+        return 10
+    elif decimal_number < 2:
+        return decimal_number
+    binary_number = decimal_number % 2
+    power = 1
+    decimal_number //= 2
+    is_zero = False
+    while decimal_number >= 2:
+        unity = decimal_number % 2        
+        binary_number = binary_number + \
+            (unity * (10 ** power) if unity != 0 else 10 ** (power + (1 if unity == 0 else 0))) - \
+            (10 ** power if is_zero else 0)
+        is_zero = True if unity == 0 else False
+        decimal_number //= 2
+        power += 1
+    binary_number = binary_number + decimal_number * (10 ** power) - (10 ** power if is_zero else 0)
+
+    return binary_number
