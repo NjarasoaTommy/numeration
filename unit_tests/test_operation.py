e0e30cd58665627e0933b97c0dfb4b883ff815d1
Njarasoa Tommy <njarasoatommy@gmail.com>
2026-04-10 11:45:40 +0300

    Add a test for the function which makes addition of any two binary numbers.

--- unit_tests/test_operation.py
+++ unit_tests/test_operation.py
@@ -1,4 +1,5 @@
 from outils.operation import *
+from outils.transform import *
 def test_split_number():
     assert split_number(12345) == [1, 2, 3, 4, 5]
     assert split_number(2904) == [2, 9, 0, 4]
@@ -17,3 +18,12 @@ def test_join_number():
     assert join_number([5, 4, 2, 0, 0, 1, 7]) == 5420017
     assert join_number([5, 4, 2, 0, 0, 1, 7]) == 5420017
     assert join_number([5, 0, 0, 0, 0, 9, 3]) == 5000093
+
+def test_addition():
+    assert addition(decimal_to_binary(3), decimal_to_binary(1)) == decimal_to_binary(3 + 1)
+    assert addition(decimal_to_binary(12), decimal_to_binary(5)) == decimal_to_binary(12 + 5)
+    assert addition(100, 10) == 110
+    assert addition(100, 1) == 101
+    assert addition(11, 11) == 110
+    assert addition(decimal_to_binary(45), decimal_to_binary(5)) == decimal_to_binary(45 + 5)
+    assert addition(decimal_to_binary(133), decimal_to_binary(300)) == decimal_to_binary(133 + 300)
