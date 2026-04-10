d2eee9077d361043a260b12ebe6147bbdd5d5ef4
Njarasoa Tommy <njarasoatommy@gmail.com>
2026-04-10 11:21:54 +0300

    Add a test for the function which transform decimal number into a binary number.

--- unit_tests/test_transform.py
+++ unit_tests/test_transform.py
@@ -3,3 +3,9 @@ def test_binary_to_decimal():
     assert binary_to_decimal(10) == 2
     assert binary_to_decimal(111) == 7
     assert binary_to_decimal(1111) == 15
+
+def test_decimal_to_binary():
+    assert decimal_to_binary(2) == 10
+    assert decimal_to_binary(16) == 10000
+    assert decimal_to_binary(3) == 11
+    assert decimal_to_binary(7) == 111
